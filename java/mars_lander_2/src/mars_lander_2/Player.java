53fbfcbceffacde2f25231b35c1eef55911d365f
xiaohouzi <pierre.guerinet@gmail.com>
2017-01-28 15:42:43 +0100

    tools methods re-implemented in java.
    on going.
    misses main method.

--- java/mars_lander_2/src/mars_lander_2/Player.java
+++ java/mars_lander_2/src/mars_lander_2/Player.java
@@ -2,24 +2,79 @@ package mars_lander_2;
 
 import java.util.Scanner;
 
-class Player {
+class Point	{
+	public int x;
+	public int y;
+}
+
+class Ship 	{
+	double r;
+	int throttle;
+	Point pos;
+}
+
+public class Player {
+
+	private Point[] surface;
+	private int peak;
 	
-	class Point	{
-		public int x;
-		public int y;
+	private Point landing;
+	
+	/*
+	 *  constructs the world map & gets the highest peak
+	 *  initializes array Point[] surface 
+	 *  initializes attributes peak
+	 */
+	private void constructSurfaceAndPeak(Scanner in) {
+		int surfaceN = in.nextInt(); // the number of points used to draw the surface of Mars.		
+		surface = new Point[surfaceN];
+		int highest_point = 0;
+		for (int i = 0; i < surfaceN; i++) {
+			Point p = new Point();
+			p.x = in.nextInt();
+			p.y = in.nextInt();
+			
+			if (highest_point < p.y) {
+				highest_point = p.y;
+			}			
+			surface[i] = p;
+		}
+		peak = highest_point;
 	}
 	
-	class Ship 	{
-		double r;
-		int throttle;
-		Point pos;
+	/*
+	 * construct the landing point (ie middle of the landing zone)
+	 */
+	private void constructLandingPoint() throws Exception {
+		int indexOfLandingZone = landingZone(surface);
+		landing = new Point();
+		landing.x = (surface[indexOfLandingZone].x + surface[indexOfLandingZone - 1].x) / 2;
+		landing.y = surface[indexOfLandingZone].y;
 	}
 	
+	public Player(Scanner in) throws Exception {
+		constructSurfaceAndPeak(in);
+		constructLandingPoint();
+	}
+		
+	/*
+	 * Check wheter p in on segment AB
+	 * @param p point to check
+	 * @param a a coord of AB segment
+	 * @param b b coord of AB segment
+	 * @return true if p in on segment AB
+	 */
 	public boolean isOnSegment(Point p, Point a, Point b) {
 		return (p.x <= Math.max(a.x, b.x) && p.x >= Math.min(a.x, b.x) &&
 				p.y >= Math.min(a.y, b.y) && p.y <= Math.max(a.y, b.y));
 	}
 	
+	/*
+	 * Return the slope of segment AB
+	 * @param a a coord of AB segment
+	 * @param b b coord of AB segment
+	 * @return the slope of AB semgent or 0 if divideBy0
+	 */
 	public int slope(Point a, Point b) {
 		int diff = b.y - a.y;
 		if (diff != 0) {
@@ -103,40 +158,39 @@ class Player {
 	public double angle(double vspeed, double power) {
 		return Math.acos(power / vspeed) * 180.0 * Math.PI;
 	}
+	
+	/*
+	 * @param X x-pos of the module
+	 * @param Y y-pos of the module
+	 * @param hSpeed the horizontal speed (in m/s), can be negative.
+	 * @param vSpeed the vertical speed (in m/s), can be negative.
+	 * @param fuel the quantity of remaining fuel in liters.
+	 * @param rotate the rotation angle in degrees (-90 to 90).
+	 * @param power the thrust power (0 to 4).
+	 */
+	public void land(int X, int Y, int hSpeed, int vSpeed, int fuel, int rotate, int power) {
+		System.err.println("20 3 ");
+	}
 
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
-		int surfaceN = in.nextInt(); // the number of points used to draw the
-										// surface of Mars.
-		for (int i = 0; i < surfaceN; i++) {
-			int landX = in.nextInt(); // X coordinate of a surface point. (0 to
-										// 6999)
-			int landY = in.nextInt(); // Y coordinate of a surface point. By
-										// linking all the points together in a
-										// sequential fashion, you form the
-										// surface of Mars.
+		Player p = null;
+		try {
+			p = new Player(in);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-
-		// game loop
+		
 		while (true) {
 			int X = in.nextInt();
 			int Y = in.nextInt();
-			int hSpeed = in.nextInt(); // the horizontal speed (in m/s), can be
-										// negative.
-			int vSpeed = in.nextInt(); // the vertical speed (in m/s), can be
-										// negative.
-			int fuel = in.nextInt(); // the quantity of remaining fuel in
-										// liters.
-			int rotate = in.nextInt(); // the rotation angle in degrees (-90 to
-										// 90).
-			int power = in.nextInt(); // the thrust power (0 to 4).
-
-			// Write an action using System.out.println()
-			// To debug: System.err.println("Debug messages...");
-
-			// rotate power. rotate is the desired rotation angle. power is the
-			// desired thrust power.
-			System.out.println("-20 3");
+			int hSpeed = in.nextInt();
+			int vSpeed = in.nextInt(); 
+			int fuel = in.nextInt(); 
+			int rotate = in.nextInt(); 
+			int power = in.nextInt(); 
+			
+			p.land(X, Y, hSpeed, vSpeed, fuel, rotate, power);
 		}
 	}
 }
