afb8526a4d3e9fe654bc2e84e6a7eb0d08bdb98d
xiaohouzi <pierre.guerinet@gmail.com>
2017-03-09 23:11:29 +0100

    cleaned up from old a star version new one is working now

--- py/le_labyrinthe/snippet.py
+++ py/le_labyrinthe/snippet.py
@@ -55,73 +55,6 @@ class Node:
     
     def __str__(self):
         return str(self.plot)
-    
-class AstarOld:
-     
-    def __init__(self):
-        self.iter = 0
-     
-    def manhattan_distance(self, a, b):
-        return abs(b.x - a.x) + abs(b.y - a.y)
-     
-    def search(self, graph, start, goal):
-        node_goal = Node(goal)
-        closeList = []
-        current = Node(start)
-        openList = [current]
-        self.iter = 0
-         
-        while openList:
-             
-            self.iter += 1            
-            current = min(openList, key=lambda inst: inst.f)
-            openList.remove(current)
-            
-            print("%d, %s: %d" % (self.iter, current, current.f), file=sys.stderr)   
-            
-            neighbours = graph[current]
-            for neighbour in neighbours:
-                if not neighbour.isDiscovered:                    
-                    continue
- 
-                node = Node(neighbour)                      
-                node.g = current.g + self.manhattan_distance(current.plot, node.plot)
-                node.h = self.manhattan_distance(node.plot, node_goal.plot)
-                node.f = node.h + node.g
-                node.x = current.x + 1                
-                node.parent = current           
- 
-                if node == node_goal:
-                    print("nbIter= ", self.iter, file=sys.stderr)                    
-                    return node
-                if self.worthTrying(node, openList, closeList):
-                    openList.append(node)
-            closeList.append(current)
-             
-    def worthTrying(self, newNode, openList, closeList):
-        for element in openList:
-            if element == newNode:
-                if element.f < newNode.f:
-                    return False
-                else:
-                    openList.remove(element)
-                    break
-        for element in closeList:
-            if element == newNode:
-                if element.f < newNode.f:
-                    return False
-                else:
-                    closeList.remove(element)
-                    break
-        return True
- 
-    def getPath(self, node):
-        ptr = node
-        path = []
-        while ptr:            
-            path.append(ptr)            
-            ptr = ptr.parent
-        return path    
 
 class Astar:
       
@@ -150,7 +83,6 @@ class Astar:
             del nodeScores[current]         
             
             self.iter += 1
-            print("%d, %s: %d" % (self.iter, current, f), file=sys.stderr)   
               
             neighbours = graph[current]
             for neighbour in neighbours:
@@ -163,9 +95,7 @@ class Astar:
                     node.x = current.x + 1                
                     node.parent = current           
   
-                    if node == node_goal:
-                        #print("nbIter= ", self.iter, file=sys.stderr)
-                        print("nbIter= ", self.iter, file=sys.stderr)                          
+                    if node == node_goal:                         
                         return node
                   
                     if self.worthTrying(node, nodeScores, closeList, heapl):
@@ -252,13 +182,9 @@ class Maze:
         return back_trace_dict.get(command)
         
     def getNextCoordinates(self, stringMaze, stack):
-
-        print("getNextCoordinates", file=sys.stderr)
         self.already.append(self.current)
-
-        print("current position: ", self.current, file=sys.stderr)        
+ 
         nextMoves = self.graph[self.current]
-        print("possible moves: ", nextMoves, file=sys.stderr)
         for nextMove in nextMoves:
             if not nextMove in self.already:
                 if stringMaze[nextMove.y][nextMove.x] == self.COMMAND: #we don't want to go onto the command center at this time
@@ -280,9 +206,6 @@ maze = Maze()
 # c: number of columns.
 # a: number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
 r, c, a = [int(i) for i in input().split()]
-#print("r=%d, c=%d, a=%d" % (r, c, a), file=sys.stderr)
-#r, c, a = 15, 30, 70
-#r, c, a = 15, 30, 39
 maze.width = c
 maze.height = r
 maze.rounds = a
@@ -291,58 +214,18 @@ stack = []
 path = None
 phase = Phases.DISCOVER
 astar = Astar()
-"""
-inputx = [
-'##############################',
-'#............................#',
-'#.#######################.#..#',
-'#.....T.................#.#..#',
-'#.....#.................#.#..#',
-'#.#######################.#..#',
-'#.....##......##......#....###',
-'#...####..##..##..##..#..#...#',
-'#.........##......##.....#...#',
-'###########################.##',
-'#......#......#..............#',
-'????C..#.....................#',
-'????#..####################..#',
-'????.........................#',
-'?????#########################']
- """
- 
-inputx = ['##############################',
-'#TX..........................#',
-'##...........................#',
-'#............................#',
-'#............................#',
-'#............................#',
-'#............................#',
-'#............................#',
-'#............................#',
-'#............................#',
-'#............................#',
-'#............................#',
-'#............................#',
-'#...........................C#',
-'##############################']
 
 
 # game loop
 while True:
     
-    start = time.time()
-                
+    start = time.time()              
     kr, kc = [int(i) for i in input().split()]    
-    #kr, kc = 13, 6
-    #kr, kc = 1,1
-    
-    #print("kr=%d, kc=%d"  % (kr, kc), file=sys.stderr)
+
     maze.current = Plot(kc, kr)
     stringMaze = []    
     for i in range(r):
         row = input()  # C of the characters in '#.TC?' (i.e. one line of the ASCII maze).
-        #row = inputx[i]
-        #print(row, file=sys.stderr)
         x = row.find('C')
         if x != -1:
             maze.command = Plot(x, i)
@@ -353,20 +236,14 @@ while True:
     
     maze.buildGraph(stringMaze)
     
-    current = time.time()
-    print("after buildGraph = ", (current-start) * 1000.0, file=sys.stderr)
-    
+    current = time.time()  
     if phase == Phases.DISCOVER:
 
         # check if C is reachable
         # compute way length from command center to start
-        print("CCCCCCCCCCCCCCCCCCCCC", maze.command, file=sys.stderr)
         if maze.command:
             path = deque()
-            node = astar.search(maze.graph, maze.start, maze.command)
-            
-            # debug purpose only
-            
+            node = astar.search(maze.graph, maze.start, maze.command)            
             if node: # not path yet
                 
                 if node.x <= maze.rounds: # means we have short path
@@ -382,33 +259,17 @@ while True:
                     togoal.pop()
                     
                     path.extend(togoal)
-                    #print("current= ", maze.current, file=sys.stderr)
-                    #print(path, file=sys.stderr)
-                    #print("path_to_goal", maze.command, file=sys.stderr)
-                    #print(path, file=sys.stderr)
-                    
-                    #print("path_to_goal: ", path_to_goal, file=sys.stderr)
-                    #print("path: ", path, file=sys.stderr)
 
                     #join both pathes                    
                     phase = Phases.DISPLAY
-                
-                    #exit() 
-                    
-            # debug purpose
+
 
         if phase == Phases.DISCOVER: # if phase is still discover, means we do not have found shortest path
-            print("phase DISCOVER", file=sys.stderr)
             direction = maze.getNextCoordinates(stringMaze, stack)
-            print("-> %s" % direction, file=sys.stderr)
             if direction:    
                 print(direction)
     
     if phase == Phases.DISPLAY:
-        #print("phase DISPLAY", file=sys.stderr)
-        #print("%s %s" % stack[-1], file=sys.stderr)
-        #print("current= ", maze.current, file=sys.stderr)
-        #print("stack= ", stack, file=sys.stderr)
         print(maze.toDirection(maze.current, path.pop().plot))        
 
     end = time.time()
